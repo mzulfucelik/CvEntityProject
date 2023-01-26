0a1d6425b8928b9d6935980c704390cb3ca5b52a
mzulfucelik <muhammedzulfucelik@gmail.com>
2023-01-26 04:16:43 +0300

    Add project files.

---
+++ CvEntityProject/Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CvEntityProject
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        DBCVENTITYEntities db = new DBCVENTITYEntities();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Repeater1.DataSource = db.TBLHAKKIMDA.ToList();
+            Repeater1.DataBind();
+            Repeater2.DataSource =db.TBLHAKKIMDA.ToList();
+            Repeater2.DataBind();
+            Repeater3.DataSource = db.TBLHAKKIMDA.ToList();
+            Repeater3.DataBind();
+        }
+    }
+}
